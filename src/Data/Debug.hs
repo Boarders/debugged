43a0ab5abe55fa0dd0728629744b508af8446887
Boarders <callan.mcgill@gmail.com>
2026-03-02 11:17:27 -0500

    Add entry-point module

---
+++ src/Data/Debug.hs
@@ -0,0 +1,16 @@
+module Data.Debug
+  ( module Data.Debug.Type
+  , module Data.Debug.Class
+  , module Data.Debug.Pretty
+  , module Data.Debug.Diff
+  , module Data.Debug.Generics
+  , module Data.Debug.IO
+  ) where
+
+
+import Data.Debug.Type
+import Data.Debug.Class
+import Data.Debug.Pretty
+import Data.Debug.Diff
+import Data.Debug.Generics
+import Data.Debug.IO
